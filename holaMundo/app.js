3fc3fff9dd0d7d39ecc688777f45fd93b923dca2
Jennifer Martinez Najera <jennifer2018mn@gmail.com>
2024-09-18 11:41:47 -0600

    Comit inicial

---
+++ holaMundo/app.js
@@ -0,0 +1,7 @@
+const { otroMensaje } = require("./hola")
+
+
+console.log("Esto es el punto de entrada")
+
+
+console.log(otroMensaje)
---
+++ holaMundo/hola.js
@@ -0,0 +1,11 @@
+const mensaje = "Hola mundo"
+
+const otroMensaje = "Este es otro mensaje"
+
+console.log(mensaje)
+
+if(mensaje){
+    console.log("se imprimio el mensaje")
+}
+
+module.exports = {otroMensaje}
