3efed773c507a913847b0e17cf20f68c326e52fe
Jennifer Martinez Najera <jennifer2018mn@gmail.com>
2024-09-23 11:43:23 -0600

    Agrega el arreglo task y filtra las tareas por realizar

--- holaMundo/app.js
+++ holaMundo/app.js
@@ -1,7 +1,20 @@
-const { otroMensaje } = require("./hola")
+const tareas = [
+    {
+        nombre: "Estudiar para el examen de backend",
+        fecha: "2024-09-23",
+        hecho: false
+    },
+    {
+        nombre: "Ir de compras",
+        fecha: "2024-09-23",
+        hecho: false
+    },
+    {
+        nombre: "Hacer tarea",
+        fecha: "2024-09-22",
+        hecho: true
+    },
+];
 
-
-console.log("Esto es el punto de entrada")
-
-
-console.log(otroMensaje)
+const tareasPorRealizar = tareas.filter(tarea => !tarea.hecho);
+console.log(tareasPorRealizar);
