0299885d52b52e4d25a1c8c79296f78035e0fe36
Jennifer Martinez Najera <jennifer2018mn@gmail.com>
2024-10-03 11:54:15 -0600

    Agrega el proyecto de control_usuarios actualizado y metodos ademas de GET

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -2,9 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.get("/",(req, res) => {
-    res.status(404).send("Hola mundo!!!");
+app.get("/usuarios",(req, res) => {
+    const usuarios=[
+        {
+        id: 1,
+        nombre: "Jennifer",
+        apellido: "perez",
+        email: "jennifer@gmail.com",
+        },
+        {
+            id: 2,
+            nombre: "Ana",
+            apellido: "perez",
+            email: "ana@gmail.com",    
+        },
+    ];
+
+    res.status(200).send(usuarios);
 });
+
 app.listen(3000,()=> {
 
     console.log("Servidor corriendo en http://localhost:3000");
