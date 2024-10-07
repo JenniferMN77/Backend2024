1615697262378a6535738060db361b70be184bf0
Jennifer Martinez Najera <jennifer2018mn@gmail.com>
2024-10-07 12:06:36 -0600

    Agrega el proyecto control_usuarios con id

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -1,25 +1,32 @@
 const express = require('express');
 
 const app = express();
+const usuarios=[
+    {
+    id: 1,
+    nombre: "Jennifer",
+    apellido: "perez",
+    email: "jennifer@gmail.com",
+    },
+    {
+        id: 2,
+        nombre: "Ana",
+        apellido: "perez",
+        email: "ana@gmail.com",    
+    },
+];
 
 app.get("/usuarios",(req, res) => {
-    const usuarios=[
-        {
-        id: 1,
-        nombre: "Jennifer",
-        apellido: "perez",
-        email: "jennifer@gmail.com",
-        },
-        {
-            id: 2,
-            nombre: "Ana",
-            apellido: "perez",
-            email: "ana@gmail.com",    
-        },
-    ];
-
-    res.status(200).send(usuarios);
+    res.status(200).send({usuarios});
 });
+
+app.get("/usuarios/:id", (req,res)=>{
+    const {id} = req.params;
+
+  const usuario = usuarios.find((usuario) => usuario.id == id);
+
+    res.status(200).send(usuario);
+} );
 
 app.listen(3000,()=> {
 
