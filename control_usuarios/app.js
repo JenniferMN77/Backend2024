5f34c24ca357923b6a112629ce2eb8f373bd09ef
Jennifer Martinez Najera <jennifer2018mn@gmail.com>
2024-10-15 17:18:00 -0600

    Agraga las validaciones de los campos nombre apellido, email

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 const usuarios=[
     {
     id: 1,
     nombre: "Jennifer",
-    apellido: "perez",
+    apellido: "perez", 
     email: "jennifer@gmail.com",
     },
     {
@@ -23,13 +24,56 @@ app.get("/usuarios",(req, res) => {
 app.get("/usuarios/:id", (req,res)=>{
     const {id} = req.params;
 
+    if(isNaN(id)){
+        res.status(400).send({error: "el id debe ser un numero"});
+        return;
+    };
+
   const usuario = usuarios.find((usuario) => usuario.id == id);
 
+    if(usuario === undefined){
+        res.status(404).send({error: `El usuario con id ${id} no existe` });
+        return;
+    }
     res.status(200).send(usuario);
-} );
+} );// end point
+
+
+//crear un nuevo punto de entrada
+
+app.post("/usuarios", (req, res)=> {
+    const {nombre,apellido,email} = req.body;
+
+    //Validar los campos nombre, apellido, email
+    if (!nombre) {
+     res.status(400).json({ error: "El campo 'nombre' es obligatorio." });
+     return;
+    }
+
+    if (!apellido) {
+     res.status(400).json({ error: "El campo 'apellido' es obligatorio." });
+     return;
+    }
+
+    if (!email) {
+        return res.status(400).json({ error: "El campo 'email' es obligatorio." });
+    }
+    // Agregar el nuevo usuario si todos los campos son válidos
+    usuarios.push({ id: usuarios.length + 1, nombre, apellido, email });
+   
+    
+    res.status(201).send("El usuario se agregó correctamente");
+});
+
+   
+   // usuarios.push({id: usuarios.length + 1, nombre, apellido, email});
+
+    //res.status(201).send("El usuario se agrego correctamente");
+   // });
+
 
 app.listen(3000,()=> {
-
     console.log("Servidor corriendo en http://localhost:3000");
-
 });
+
+
