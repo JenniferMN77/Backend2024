829c65834553d2358332989bb0fe03d57b33ec47
Jennifer Martinez Najera <jennifer2018mn@gmail.com>
2024-11-25 13:18:41 -0600

    Equipo 8. Agregar los endpoints de la tabla products, products_suppliers y  suppliers.

--- stock_manager/src/server.js
+++ stock_manager/src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const usersRoutes = require('./routes/users');
 const staffRoutes = require('./routes/staff');
+const products_suppliersRoutes = require('./routes/products_suppliers'); // Importa las rutas de products_suppliers
+const suppliersRoutes = require('./routes/suppliers');
+const productsRoutes = require ('./routes/products');
+
 class Server{
     constructor() {
         this.app = express();
@@ -19,6 +23,9 @@ class Server{
     routes(){
         this.app.use('/users', usersRoutes); 
         this.app.use('/staff', staffRoutes);
+        this.app.use('/suppliers', suppliersRoutes);
+        this.app.use('/products', productsRoutes);
+        this.app.use('/products_suppliers', products_suppliersRoutes); // Rutas de products_suppliers
     }
 
     start() {
